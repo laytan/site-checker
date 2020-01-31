947024bdda27b0ac02dcbd85dea777f1d5316b71
Laytan Laats <Laytanlaats@hotmail.com>
2020-01-31 17:51:36 +0100

    Add more messages

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ const SiteDownError = require('./SiteDownError');
 
 const TELEGRAM_API_URL = `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/`;
 
+let uptime = 0;
+
 checkAll();
 setInterval(checkAll, 60000);
+setInterval(() => {
+  uptime += 3600000;
+  telegram(`I am still running! Uptime: ${uptime / 1000} hours`);
+}, 3600000);
+telegram("Bleep bloop started up!");
 
 function getSites() {
   let sitesToCheck = process.env.SITES_TO_CHECK.split(',');
@@ -26,19 +33,19 @@ function check(site) {
 
 function notifyDown(err) {
   if(err.constructor.name === 'FetchError') {
-    telegramError(err.message);
+    telegram(err.message);
     console.error("Tried to ping site that does not exist");
   } else if(err.constructor.name === 'SiteDownError') {
-    telegramError(err);
+    telegram(err.message);
     console.log("Site is down", err);
   } else {
-    telegramError(err.message);
+    telegram(err.message);
     console.error(err);
   }
 }
 
-function telegramError(err) {
-  fetch(`${TELEGRAM_API_URL}sendMessage?chat_id=${process.env.TELEGRAM_CHAT_ID}&text=${err.message}`)
+function telegram(message) {
+  fetch(`${TELEGRAM_API_URL}sendMessage?chat_id=${process.env.TELEGRAM_CHAT_ID}&text=${message}`)
     .catch(console.error);
 }
 
